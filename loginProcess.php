91f5888580ae56d1bc68934d1b3c247c0124fdff
DiegoSynth <foxy_dcc@hotmail.com>
2016-07-22 23:14:08 +0200

    Add files via upload

--- loginProcess.php
+++ loginProcess.php
@@ -1,26 +1,19 @@
 <?php
-    require_once 'LoginAdmin.php';
+  require_once 'LoginAdmin.php';
 
 	$loginAdmin = new LoginAdmin();
 
-    if($_SERVER['REQUEST_METHOD'] == "POST")
+  if($_SERVER['REQUEST_METHOD'] == "POST")
 	{
-        $messageRequest = json_decode(file_get_contents('php://input'), true);
-        if($messageRequest['messageName'] == "processLogin")
-		{
-            $messageResponse = $loginAdmin->loginCheck($messageRequest['nameUser'], $messageRequest['secret']);
-        }
-		else
-		{
-            $messageResponse = array("status" => -2, "msg" => "There are some issues with the sent message structure!");
-        }
-    }
+    $messageRequest = json_decode(file_get_contents('php://input'), true);
+    $messageResponse = $loginAdmin->loginCheck($messageRequest['username'], $messageRequest['password']);
+  }
 	else
 	{
-        $messageResponse = array("status" => -1, "msg" => "Request method not accepted only POST messages are accepted!");
-    }
+    $messageResponse = array("status" => -1, "msg" => "Request method not accepted only POST messages are accepted!");
+  }
 
-    /* Output header */
-    header('Content-type: application/json');
-    echo json_encode($messageResponse);
+  /* Output header */
+  header('Content-type: application/json');
+  echo json_encode($messageResponse);
 ?>
