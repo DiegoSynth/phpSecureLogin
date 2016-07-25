4412389819cadb2ebc93f166040ca77c96a4bae1
DiegoSynth <foxy_dcc@hotmail.com>
2016-07-25 12:56:55 +0200

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -22,13 +22,38 @@ function validate()
 function send()
 {
     var sMsg = validate();
+    var sUrl;
     if (sMsg.length == 0)
     {
         document.getElementById("hPassword").value = hex_sha512(document.getElementById("txtPassword").value);
-        document.getElementById("frmLogin").submit();
+        //document.getElementById("frmLogin").submit();
+        sUrl = "loginProcess.php?txtEmail=" + document.getElementById("txtEmail").value + "hPassword=" + document.getElementById("hPassword").value;
+        getFromUrl(sUrl, processLoginResult);
     }
     else
     {
         alert(sMsg);
     }
+}
+
+function getFromUrl(sUrl, callbackfunction)
+{
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function ()
+    {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200)
+        {
+            if (callbackfunction)
+            {
+                callbackfunction(xmlhttp);
+            }
+        }
+    };
+    xmlhttp.open("GET", sUrl, true);
+    xmlhttp.send();
+}
+
+function processLoginResult(xmlhttp)
+{
+    alert(xmlhttp.responseText);
 }
