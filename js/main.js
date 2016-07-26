e2e2ca028318a8026e8245578eea2254d3989e82
DiegoSynth <foxy_dcc@hotmail.com>
2016-07-26 13:04:46 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -23,7 +23,6 @@ function getSessionData()
 
 function validateSession(xmlhttp)
 {
-    alert(xmlhttp.responseText);
     var userLogged = eval("(" + xmlhttp.responseText + ")");
     if(userLogged.userId == -1)
     {
